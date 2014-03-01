03f1f12d061a4c0a50594241051952edee96bbd3
Jaiden King <king.jaiden@yahoo.com>
2014-03-01 00:24:52 -0800

    Gut the .java
    Removed existing code so that I could refactor it

--- Coord.java
+++ Coord.java
@@ -2,204 +2,26 @@ package king.jaiden.RATL;
 
 public class Coord {
 	private double x,y,z;
-
-	/**
-	 * Creates a new Coord Object
-	 * @param x X Coordinate
-	 * @param y Y Coordinate
-	 * @param z Z Coordinate
-	 */
-	public Coord(double x, double y, double z){
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-	/**
-	 * Creates a new Coord Object with a z of 0.
-	 * @param angle degrees from the positive x axis.
-	 * @param radius distance from origin.
-	 */
-	public Coord(double angle, double radius){
-		z = 0;
-		y = Math.sin(Math.toRadians(angle)) * radius;
-		x = Math.cos(Math.toRadians(angle)) * radius;
-	}
+	public Coord(double x, double y, double z){}
+	public Coord(double angle, double radius){}
 	
-	public Coord(double angle, double radius, boolean flat){
-		z = Math.sin(Math.toRadians(angle)) * radius;
-		y = 0;
-		x = Math.cos(Math.toRadians(angle)) * radius;
-	}
-	
-	/**
-	 * Returns X coordinate
-	 * @return X coordinate
-	 */
-	public double getX() {
-		return x;
-	}
-	/**
-	 * Sets the X coordinate
-	 * @param x The new X coordinate
-	 */
-	public void setX(double x) {
-		this.x = x;
-	}
-
-	/**
-	 * Returns Y coordinate
-	 * @return Y coordinate
-	 */
-	public double getY() {
-		return y;
-	}
-	/**
-	 * Sets the Y coordinate
-	 * @param y The new Y coordinate
-	 */
-	public void setY(double y) {
-		this.y = y;
-	}
-
-	/**
-	 * Returns Z coordinate
-	 * @return Z coordinate
-	 */
-	public double getZ() {
-		return z;
-	}
-	/**
-	 * Sets the Z coordinate
-	 * @param z The new Z coordinate
-	 */
-	public void setZ(double z) {
-		this.z = z;
-	}
-	/**
-	 * Compares the Coord with the given Coord
-	 * @param c
-	 * @return True if both Coords have equal coordinates
-	 */
-	public boolean equals(Coord c){
-		if(c.getX()==x&&c.getY()==y&&c.getZ()==z)
-			return true;
-		else
-			return false;
-	}
-	/**
-	 * Adds the given Coord's coordinates to this Coord's coordinates
-	 * @param c
-	 * @return Returns itself.
-	 */
-	public Coord add(Coord c){
-		x += c.getX();
-		y += c.getY();
-		z += c.getZ();
-		return this;
-	}
-	/**
-	 * Subtracs the given Coord's coordinates from this Coord's coordinates
-	 * @param c
-	 * @return Returns itself.
-	 */
-	public Coord subtract(Coord c){
-		x -= c.getX();
-		y -= c.getY();
-		z -= c.getZ();
-		return this;
-	}
-	/**
-	 * Calculates the radius of the Coord.
-	 * @return Radius from the origin as a positive number.
-	 */
-	public double getRadius(){
-		return Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
-	}
-	
-	/**
-	 * Copies this Coord
-	 * @return A Coord with the same coordinates
-	 */
-	public Coord copy(){
-		return new Coord(x,y,z);
-	}
-	/**
-	 * Sets the x coordinate to the specified value while keeping the ratio of x:y consistent.
-	 * Ignores the z coordinate.
-	 * @param x New x coordinate
-	 */
-	public void setXconstrained2D(double x){
-		double b = y / this.x;
-		this.x = x;
-		y = b*x;
-	}
-
-	/**
-	 * Sets the y coordinate to the specified value while keeping the ratio of x:y consistent.
-	 * Ignores the z coordinate.
-	 * @param y New y coordinate
-	 */
-	public void setYconstrained2D(double y){
-		double b = this.y / this.x;
-		this.y = y;
-		x = b*y;
-	}
-	
-	/**
-	 * Sets the radius to the specified value while keeping the ratio of x:y consistent.
-	 * Ignores the z coordinate.
-	 * @param radius The new radius.
-	 */
-	public void setRadiusConstrained2D(double radius){
-		double theta = Math.atan(y/x);
-		if(x<0){
-			theta+=Math.PI;
-		}
-		x = Math.cos(theta)*radius;
-		y = Math.sin(theta)*radius;
-	}
-
-	/**
-	 * Sets the radius to the specified value while keeping the ratio of x:z consistent.
-	 * Ignores the y coordinate.
-	 * @param radius The new radius.
-	 */
-	public void setRadiusConstrained2DZ(double radius){
-		double theta = Math.atan(z/x);
-		if(x<0){
-			theta+=Math.PI;
-		}
-		x = Math.cos(theta)*radius;
-		z = Math.sin(theta)*radius;
-	}
-	
-	/**
-	 * Returns the angle in radians of this vector (2D).
-	 * @return The angle in radians.
-	 */
-	public double getAngle(){
-		double a = Math.atan(y/x);
-		if(x<0)
-			a += Math.PI;
-		return a;
-	}
-	/**
-	 * Returns the angle in radians of this vector (2DZ).
-	 * @return The angle in radians.
-	 */
-	public double getAngle2DZ(){
-		double a = Math.atan(z/x);
-		if(x<0)
-			a += Math.PI;
-		return a;
-	}
-	/**
-	 * Sets the angle of this vector, keeping the radius constant. 2DZ
-	 * @param radians the new angle, in radians.
-	 */
-	public void setAngle(double radians){
-		double r = getRadius();
-		x = Math.cos(radians) * r;
-		z = Math.sin(radians) * r;
-	}
+	public Coord(double angle, double radius, boolean flat){}
+	public double getX() {}
+	public void setX(double x) {}
+	public double getY() {}
+	public void setY(double y) {}
+	public double getZ() {}
+	public void setZ(double z) {}
+	public boolean equals(Coord c){}
+	public Coord add(Coord c){}
+	public Coord subtract(Coord c){}
+	public double getRadius(){}
+	public Coord copy(){}
+	public void setXconstrained2D(double x){}
+	public void setYconstrained2D(double y){}
+	public void setRadiusConstrained2D(double radius){}
+	public void setRadiusConstrained2DZ(double radius){}
+	public double getAngle(){}
+	public double getAngle2DZ(){}
+	public void setAngle(double radians){}
 }
--- MyWindow.java
+++ MyWindow.java
@@ -1,98 +1,15 @@
-package king.jaiden.RATL;
-
-import static org.lwjgl.opengl.GL11.*;
-
-import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.util.glu.GLU;
-
 public abstract class MyWindow {
 	public int WINDOW_WIDTH, WINDOW_HEIGHT, tick;
+	public MyWindow(){
+		This assigns default width, height, fov, etc by calling another constructor
+	} 
 	
-	/**
-	 * Creates a new LWJGL window with GL_BLEND, GL_DEPTH_TEST, and GL_CULL_FACE enabled
-	 * in a 3d environment.
-	 * @param w Window width
-	 * @param h Window height
-	 * @param fov Vertical(?) field of view
-	 * @param title The title of the window
-	 */
-	public MyWindow(int w,int h, int fov, String title, boolean fullscreen, boolean matrixMode){
-		WINDOW_WIDTH = w;
-		WINDOW_HEIGHT = h;
-		// Setup the Display
-		try {
-			DisplayMode displayMode = null;
-	        DisplayMode[] modes = Display.getAvailableDisplayModes();
-	        for (int i = 0; i < modes.length; i++)
-	        {
-	        	if (modes[i].getWidth() == w
-	            && modes[i].getHeight() == h
-	            && modes[i].isFullscreenCapable())
-	        		{
-	            	 	displayMode = modes[i];
-	        		}
-	        }
-	        if(!fullscreen){
-	        	displayMode = new DisplayMode(w,h);
-	        }
-			Display.setDisplayMode(displayMode);
-			Display.setFullscreen(fullscreen);
-			Display.create();
-			Display.setTitle(title);
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		
-		if(matrixMode){
-			setup3DMatrix();
-		}else{
-			setup2DMatrix(w,h);
-		}
-		enableTests();
-		init();
-		
-		while (!Display.isCloseRequested()){
-			input();
-			tick();
-			draw();
-			
-			Display.update();
-			Display.sync(60);
-		}
-		Display.destroy();
-	}
+	public MyWindow(int w,int h, int fov, String title, boolean fullscreen, boolean matrixMode){}
 	public abstract void init();
 	public abstract void input();
-	public void tick(){
-		tick++;
-	}
-	public void draw(){
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	}
-	public void setup3DMatrix(){
-		glViewport(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
-		// Setup the Matrix
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		GLU.gluPerspective(100, WINDOW_WIDTH/(float)WINDOW_HEIGHT, 0.1f, 10000);
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-	}
-	public void setup2DMatrix(int w, int h){
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrtho(0, w, 0, h, 1, -1);
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-	}
-	public void enableTests(){
-		glEnable(GL_BLEND);
-		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);	
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LESS);
-		glEnable(GL_CULL_FACE);
-	}
+	public void tick(){}
+	public void draw(){}
+	public void setup3DMatrix(){}
+	public void setup2DMatrix(int w, int h){}
+	public void enableTests(){}
 }
